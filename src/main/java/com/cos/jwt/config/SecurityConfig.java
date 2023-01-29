bc28e77254aff77f1a0d4fcf6bfff37639229a95
EJ <hanej93@naver.com>
2023-01-29 15:56:24 +0900

    jwt를 위한 로그인 시도

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -2,13 +2,16 @@ package com.cos.jwt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.SecurityContextHolderFilter;
 import org.springframework.web.filter.CorsFilter;
 
+import com.cos.jwt.config.jwt.JwtAuthenticationFilter;
 import com.cos.jwt.filter.MyFilter3;
 
 import lombok.RequiredArgsConstructor;
@@ -30,6 +33,9 @@ public class SecurityConfig  {
 	        .addFilter(corsFilter) // @CrossOrgin(인증 x), 시큐리티 필터에 등록 인증(o)
 	    	.formLogin().disable() // formLogin을 안쓰겠다.
 	    	.httpBasic().disable() // 요청시 헤더에 Authorization(id,pw)를 담아서 보냄 -> 사용 안하겠다. // id,pw 대신 토큰 사용 -> Bearer 방식
+	    	.apply(new MyCustomDsl())
+//	    	.addFilter(new JwtAuthenticationFilter()) // AuthenticationManager
+	    	.and()
 	    	.authorizeRequests()
 	        	.antMatchers("/api/v1/user/**").hasAnyRole("ROLE_USER", "ROLE_MANAGER", "ROLE_ADMIN")
 	        	.antMatchers("/api/v1/manager/**").hasAnyRole("ROLE_MANAGER", "ROLE_ADMIN")
@@ -39,5 +45,17 @@ public class SecurityConfig  {
         return http.build();
     }
 	
+	
+	public class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+			http
+					.addFilter(corsFilter)
+					.addFilter(new JwtAuthenticationFilter(authenticationManager))
+//					.addFilter(new JwtAuthorizationFilter(authenticationManager, userRepository))
+					;
+		}
+	}
 
 }
--- src/main/java/com/cos/jwt/model/Users.java
+++ src/main/java/com/cos/jwt/model/Users.java
@@ -13,7 +13,7 @@ import lombok.Data;
 
 @Data
 @Entity
-public class User {
+public class Users {
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
